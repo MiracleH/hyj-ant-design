57032b7dfc92d8dd858952560473a396755f8b7c
hyj <627008263@qq.com>
2018-08-22 18:41:11 +0800

    update

--- src/components/BaseList/index.js
+++ src/components/BaseList/index.js
@@ -1,17 +1,20 @@
 import React, { PureComponent, Fragment } from 'react';
 import { Link } from 'dva/router';
 import {
-    Row,
-    Col,
-    Card,
-    Form,
-    Input,
-    Select,
-    Button,
-    DatePicker,
-    Table,
-    Divider,
-  } from 'antd';
+  Row,
+  Col,
+  Card,
+  Form,
+  Input,
+  Select,
+  Button,
+  DatePicker,
+  Table,
+  Divider,
+  Dropdown,
+  Icon,
+  Menu,
+} from 'antd';
 import styles from './index.less';
 
 const FormItem = Form.Item;
@@ -19,30 +22,29 @@ const { Option } = Select;
 const { RangePicker } = DatePicker;
 
 @Form.create()
-
 export default class BaseList extends PureComponent {
   constructor(props) {
     super(props);
 
     this.state = {
-        formValues: {},
-        searchParams: {
-            pageSize: 30
-        }
+      formValues: {},
+      searchParams: {
+        pageSize: 30,
+      },
     };
   }
 
   componentDidMount() {
-      this.searchLists()
+    this.searchLists();
   }
 
   searchLists = () => {
     let value = {
-        ...this.state.formValues,
-        ...this.state.searchParams
-    }
-    this.props.searchHandle(value)
-  }
+      ...this.state.formValues,
+      ...this.state.searchParams,
+    };
+    this.props.searchHandle(value);
+  };
 
   handleFormReset = () => {
     const { form } = this.props;
@@ -53,7 +55,7 @@ export default class BaseList extends PureComponent {
     this.searchLists();
   };
 
-  setSearch = (page) => {
+  setSearch = page => {
     this.setState(
       {
         searchParams: page,
@@ -61,149 +63,205 @@ export default class BaseList extends PureComponent {
       this.searchLists
     );
   };
-  setSize = (pageSize) => {
-    this.setState({
-      searchParams: pageSize,
-    }, this.searchLists)
+  setSize = pageSize => {
+    this.setState(
+      {
+        searchParams: pageSize,
+      },
+      this.searchLists
+    );
   };
 
-  handleSearch = (e) => {
+  handleSearch = e => {
     e.preventDefault();
     const { form } = this.props;
     const { searchParams } = this.state;
     form.validateFields((err, fieldsValue) => {
-        if (err) return;
+      if (err) return;
 
-        //todo 处理时间
+      //todo 处理时间
 
-        const values = {
-            ...fieldsValue,
-            ...searchParams
-        };
+      const values = {
+        ...fieldsValue,
+        ...searchParams,
+      };
 
-        this.setState({
-            formValues: values,
-        },this.searchLists);
+      this.setState(
+        {
+          formValues: values,
+        },
+        this.searchLists
+      );
     });
   };
 
-  getColumns = (tableTitle) => {
+  getColumns = tableTitle => {
     let arr = [];
-    tableTitle && tableTitle.length && tableTitle.forEach(val => {
+    tableTitle &&
+      tableTitle.length &&
+      tableTitle.forEach(val => {
         let obj = {};
         obj.title = val.name;
         obj.dataIndex = val.value;
         arr.push(obj);
-    })
-    if(this.props.handleOption.show) {
-        let handleObj = {}
-        handleObj.title = this.props.handleOption.titleName;
-        handleObj.dataIndex = this.props.handleOption.value;
-        handleObj.render = (dataIndex, record) => {
-            return (
-                <Fragment>
-                    {
-                        this.props.handleOption.data.map((hVal,index) => {
-                            if(hVal.url && this.props.handleOption.data.length != (index + 1)) {
-                                return (
-                                    <Fragment key={index}>
-                                        <Link key={index} to={`${hVal.url}/${dataIndex}`}>{hVal.name}</Link>
-                                        <Divider type="vertical" />
-                                    </Fragment>
-                                )
-                            }else if(hVal.todo && hVal.url && this.props.handleOption.data.length != (index + 1)) {
-                                return (
-                                    <Fragment key={index}>
-                                        <a href='javascript:void(0);' key={index}  onClick={hVal.todo.bind(this,dataIndex, this.searchLists)}>删除</a>
-                                        <Divider type="vertical" />
-                                    </Fragment>
-                                )
-                            }else if(hVal.url) {
-                                return (
-                                    <Link key={index} to={`${hVal.url}/${dataIndex}`}>{hVal.name}</Link>
-                                )
-                            }else if(hVal.todo) {
-                                return (
-                                    <a href='javascript:void(0);' key={index}  onClick={hVal.todo.bind(this,dataIndex, this.searchLists)}>删除</a> 
-                                )
-                            }
-                        })
-                    }
-                </Fragment>
-            )
-            
-        }
-        arr.push(handleObj);
+      });
+    if (this.props.handleOption.show) {
+      let handleObj = {};
+      handleObj.title = this.props.handleOption.titleName;
+      handleObj.dataIndex = this.props.handleOption.value;
+      handleObj.render = (dataIndex, record) => {
+        return (
+          <Fragment>
+            {(() => {
+              if (this.props.handleOption.data.length > 3) {
+                const menu = (
+                  <Menu>
+                    {this.props.handleOption.data.map((sVal, sIndex) => {
+                      if (sVal.url) {
+                        return (
+                          <Menu.Item key={sVal.name}>
+                            <Link key={sIndex} to={`${sVal.url}/${dataIndex}`}>
+                              {sVal.name}
+                            </Link>
+                          </Menu.Item>
+                        );
+                      } else if (sVal.todo) {
+                        return (
+                          <Menu.Item key={sVal.name}>
+                            <a
+                              href="javascript:void(0);"
+                              key={sIndex}
+                              onClick={sVal.todo.bind(this, dataIndex, this.searchLists)}
+                            >
+                              {sVal.name}
+                            </a>
+                          </Menu.Item>
+                        );
+                      }
+                    })}
+                  </Menu>
+                );
+                return (
+                  <Dropdown overlay={menu}>
+                    <a>
+                      操作 <Icon type="down" />
+                    </a>
+                  </Dropdown>
+                );
+              } else {
+                return this.props.handleOption.data.map((hVal, index) => {
+                  if (hVal.url && this.props.handleOption.data.length != index + 1) {
+                    return (
+                      <Fragment key={index}>
+                        <Link key={index} to={`${hVal.url}/${dataIndex}`}>
+                          {hVal.name}
+                        </Link>
+                        <Divider type="vertical" />
+                      </Fragment>
+                    );
+                  } else if (hVal.todo && this.props.handleOption.data.length != index + 1) {
+                    return (
+                      <Fragment key={index}>
+                        <a
+                          href="javascript:void(0);"
+                          key={index}
+                          onClick={hVal.todo.bind(this, dataIndex, this.searchLists)}
+                        >
+                          {hVal.name}
+                        </a>
+                        <Divider type="vertical" />
+                      </Fragment>
+                    );
+                  } else if (hVal.url) {
+                    return (
+                      <Link key={index} to={`${hVal.url}/${dataIndex}`}>
+                        {hVal.name}
+                      </Link>
+                    );
+                  } else if (hVal.todo) {
+                    return (
+                      <a
+                        href="javascript:void(0);"
+                        key={index}
+                        onClick={hVal.todo.bind(this, dataIndex, this.searchLists)}
+                      >
+                        {hVal.name}
+                      </a>
+                    );
+                  }
+                });
+              }
+            })()}
+          </Fragment>
+        );
+      };
+      arr.push(handleObj);
     }
     return arr;
-  }
+  };
 
   renderAdvancedForm() {
     const { form } = this.props;
     const { getFieldDecorator } = form;
     return (
-        <Form onSubmit={this.handleSearch} layout="inline">
-            <Row gutter={{ md: 8, lg: 24, xl: 48 }}>
-                {
-                    this.props.formList.map((value, index) => {
-                        return <Col md={8} sm={12} key={index}>
-                                    <FormItem label={value.name}>
-                                        {
-                                            (() => {
-                                                switch(value.type) {
-                                                    case 'input': 
-                                                        return (
-                                                            getFieldDecorator(`${value.value}`, {
-                                                            })(
-                                                                <Input style={{ width: '100%' }} placeholder={`请输入${value.name}`}/>
-                                                            )
-                                                        )
-                                                    case 'select': 
-                                                        return (
-                                                            getFieldDecorator(`${value.value}`, {
-                                                            })(
-                                                                <Select
-                                                                    placeholder={`请选择${value.name}`}
-                                                                    onChange={value.changeHandle ? value.changeHandle : null}
-                                                                    style={{ width: '100%' }}
-                                                                    >
-                                                                    {
-                                                                        value.option && value.option.map((val, key) => {
-                                                                        return <Option key={key} value={val.value}>{val.name}</Option>
-                                                                        })
-                                                                    }
-                                                                </Select>
-                                                            )
-                                                        )
-                                                    case 'rangePicker':
-                                                        return (
-                                                            getFieldDecorator(`${value.value}`, {
-                                                            })(
-                                                                <RangePicker style={{ width: '100%' }} placeholder={['开始', '结束']}/>
-                                                            )
-                                                        )
-                                                }
-                                            })()
-                                        }
-                                    </FormItem>
-                                </Col>
-                    })
-                }
-            </Row>
-            <div style={{ overflow: 'hidden' }}>
-                <span style={{ float: 'right', marginBottom: 24 }}>
-                    <Button style={{ marginLeft: 8 }} type="primary" htmlType="submit">查询</Button>
-                    <Button style={{ marginLeft: 8 }} onClick={this.handleFormReset}>重置</Button>
-                    {
-                        this.props.handleOption.add.btn ? 
-                        <Link to={this.props.handleOption.add.url}><Button style={{ marginLeft: 8 }} type="primary">{this.props.handleOption.add.btnName}</Button></Link>
-                        : null
+      <Form onSubmit={this.handleSearch} layout="inline">
+        <Row gutter={{ md: 8, lg: 24, xl: 48 }}>
+          {this.props.formList.map((value, index) => {
+            return (
+              <Col md={8} sm={12} key={index}>
+                <FormItem label={value.name}>
+                  {(() => {
+                    switch (value.type) {
+                      case 'input':
+                        return getFieldDecorator(`${value.value}`, {})(
+                          <Input style={{ width: '100%' }} placeholder={`请输入${value.name}`} />
+                        );
+                      case 'select':
+                        return getFieldDecorator(`${value.value}`, {})(
+                          <Select
+                            placeholder={`请选择${value.name}`}
+                            onChange={value.changeHandle ? value.changeHandle : null}
+                            style={{ width: '100%' }}
+                          >
+                            {value.option &&
+                              value.option.map((val, key) => {
+                                return (
+                                  <Option key={key} value={val.value}>
+                                    {val.name}
+                                  </Option>
+                                );
+                              })}
+                          </Select>
+                        );
+                      case 'rangePicker':
+                        return getFieldDecorator(`${value.value}`, {})(
+                          <RangePicker style={{ width: '100%' }} placeholder={['开始', '结束']} />
+                        );
                     }
-                </span>
-            </div>
-        </Form>
-        
-      
+                  })()}
+                </FormItem>
+              </Col>
+            );
+          })}
+        </Row>
+        <div style={{ overflow: 'hidden' }}>
+          <span style={{ float: 'right', marginBottom: 24 }}>
+            <Button style={{ marginLeft: 8 }} type="primary" htmlType="submit">
+              查询
+            </Button>
+            <Button style={{ marginLeft: 8 }} onClick={this.handleFormReset}>
+              重置
+            </Button>
+            {this.props.handleOption.add.btn ? (
+              <Link to={this.props.handleOption.add.url}>
+                <Button style={{ marginLeft: 8 }} type="primary">
+                  {this.props.handleOption.add.btnName}
+                </Button>
+              </Link>
+            ) : null}
+          </span>
+        </div>
+      </Form>
     );
   }
 
@@ -211,42 +269,38 @@ export default class BaseList extends PureComponent {
     return this.renderAdvancedForm();
   }
 
-
   render() {
-    const {
-        data, pagination, tableTitle, loading
-    } = this.props;
-    
+    const { data, pagination, tableTitle, loading } = this.props;
+
     const columns = this.getColumns(tableTitle);
     const paginationProps = {
-        showSizeChanger: true,
-        showQuickJumper: true,
-        total: pagination.total,
-        showTotal: (total, range) => {
-          return `共${total}条记录`
-        },
-        current: pagination.current,
-        defaultPageSize: 30,
-        onShowSizeChange: (current, pageSize) => {
-          this.setSize({current, pageSize});
-        },
-        onChange: (page, pageSize) => {
-          this.setSearch({ page, pageSize });
-        },
-      };
+      showSizeChanger: true,
+      showQuickJumper: true,
+      total: pagination.total,
+      showTotal: (total, range) => {
+        return `共${total}条记录`;
+      },
+      current: pagination.current,
+      defaultPageSize: 30,
+      onShowSizeChange: (current, pageSize) => {
+        this.setSize({ current, pageSize });
+      },
+      onChange: (page, pageSize) => {
+        this.setSearch({ page, pageSize });
+      },
+    };
     return (
-
-        <Card bordered={false}>
-            <div className={styles.tableList}>
-                <div className={styles.tableListForm}>{this.renderForm()}</div>
-                <Table
-                loading={loading}
-                dataSource={data}
-                columns={columns}
-                pagination={paginationProps}
-                />
-            </div>  
-        </Card>
+      <Card bordered={false}>
+        <div className={styles.tableList}>
+          <div className={styles.tableListForm}>{this.renderForm()}</div>
+          <Table
+            loading={loading}
+            dataSource={data}
+            columns={columns}
+            pagination={paginationProps}
+          />
+        </div>
+      </Card>
     );
   }
 }
--- src/routes/List/BasePage.js
+++ src/routes/List/BasePage.js
@@ -1,24 +1,24 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'dva';
-import PageHeaderLayout from '../../layouts/PageHeaderLayout';
+import PageHeaderLayout from '/layouts/PageHeaderLayout';
 import BaseList from 'components/BaseList';
 
-@connect((state) => ({
-    state
+@connect(state => ({
+  state,
 }))
 export default class BasePage extends PureComponent {
   state = {
-    op: []
+    op: [],
   };
 
-  componentDidMount () {
+  componentDidMount() {
     this.setState({
-        op: [
-            {name: '111', value: '111'},
-            {name: '222', value: '222'},
-            {name: '333', value: '333'}
-        ]
-    })
+      op: [
+        { name: '111', value: '111' },
+        { name: '222', value: '222' },
+        { name: '333', value: '333' },
+      ],
+    });
   }
 
   getList = () => {
@@ -26,71 +26,77 @@ export default class BasePage extends PureComponent {
     dispatch({
       type: 'rule/fetch',
     });
-  }
+  };
 
   deleteListItem = (id, fn) => {
     const { dispatch } = this.props;
     dispatch({
       type: 'rule/del',
       payload: {
-          id
+        id,
       },
-      callback: (res) => {
-          fn();
-      }
+      callback: () => {
+        fn();
+      },
     });
-  }
+  };
 
-  search = (values) => {
-    this.props.dispatch({
-        type: 'rule/fetch',
-        payload: values
+  search = values => {
+    const { dispatch } = this.props;
+    dispatch({
+      type: 'rule/fetch',
+      payload: values,
     });
-  }
+  };
 
   render() {
     const formList = [
-        {
-            name: '111',
-            value: '111',
-            type: 'input'
-        },
-        {
-            name: '222',
-            value: '222',
-            type: 'select',
-            option: this.state.op,
-            changeHandle: this.getList
-        },
-        {
-            name: '333',
-            value: '333',
-            type: 'rangePicker'
-        },
-    ]
+      {
+        name: '111',
+        value: '111',
+        type: 'input',
+      },
+      {
+        name: '222',
+        value: '222',
+        type: 'select',
+        option: this.state.op,
+        changeHandle: this.getList,
+      },
+      {
+        name: '333',
+        value: '333',
+        type: 'rangePicker',
+      },
+    ];
     const handleOption = {
-        show: true,
-        value: 'key',
-        titleName: '操作',
-        data: [{name: '详情', url: 'aaa-aaa'}, {name: '删除', todo: this.deleteListItem}],
-        add:{
-            btn: true,
-            url: 'page-add',
-            btnName: '新增'
-        }
-    }
-    const { list , pagination, tableTitle } = this.props.state.rule.data;
+      show: true,
+      value: 'key',
+      titleName: '操作',
+      data: [
+        { name: '详情', url: 'aaa-aaa' },
+        { name: '删除', todo: this.deleteListItem },
+        { name: '删除2', todo: this.deleteListItem },
+        { name: '详情1', url: 'aaa-aaa' },
+      ],
+      add: {
+        btn: true,
+        url: 'page-add',
+        btnName: '新增',
+      },
+    };
+    const { list, pagination, tableTitle } = this.props.state.rule.data;
     const loading = this.props.state.loading.models.rule;
     return (
       <PageHeaderLayout title="查询表格">
         <BaseList
-            formList={formList}
-            searchHandle={this.search}
-            handleOption={handleOption}
-            data={list}
-            pagination={pagination}
-            tableTitle={tableTitle}
-            loading={loading}
+          formList={formList}
+          searchHandle={this.search}
+          handleOption={handleOption}
+          data={list}
+          pagination={pagination}
+          tableTitle={tableTitle}
+          loading={loading}
         />
       </PageHeaderLayout>
     );
